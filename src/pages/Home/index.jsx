7b5a65b531de8863f85b198c24baf3e23e7d0e11
Diksha <dikshatiwari9264@gmail.com>
2025-07-17 11:27:00 +0530

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Announcementbar from "./Announcementbar";
+import Banner from "./Banner";
+import ProductCategories from "./ProductCategories";
+import ComboOffer from "./ComboOffer";
+import Testminoal from "./testminoal";
+import BestSeller from "./BestSeller";
+import Blogs from "./Blogs";
+import ChooseUs from "./ChooseUs";
+import VideoTestminol from "./VideoTestminol";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcementbar />
+      <Banner />
+      <ProductCategories />
+      <ComboOffer />
+      <Testminoal />
+      <BestSeller />
+      <Blogs />
+      <ChooseUs />
+      <VideoTestminol />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
